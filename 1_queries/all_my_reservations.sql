e8e4d682886a063aed587a260caa3dba6b402a55
mdzwink <mdzwink@gmail.com>
2022-06-05 10:01:48 -0400

    add webapp download files

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -1,7 +1,8 @@
-SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, avg(property_reviews.rating) as average_rating
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating) as average_rating
 FROM reservations
-JOIN properties ON properties.id = property_id
-JOIN property_reviews ON properties.id = property_id
-WHERE guest_id = 1
-ORDER BY start_date
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
 LIMIT 10;
